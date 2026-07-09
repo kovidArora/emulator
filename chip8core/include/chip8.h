6ac90fc4f444018572d77d31b3246362d8c5342e
Kovid Arora <kovid23arora@gmail.com>
2026-07-10 01:33:53 +0530

    starting with main stuff

--- chip8core/include/chip8.h
+++ chip8core/include/chip8.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <cstdint>
+
 class Chip8
 {
 public:
-    Chip8();
+    static constexpr int SCREEN_WIDTH = 64;
+    static constexpr int SCREEN_HEIGHT = 32;
+    static constexpr int RAM_SIZE = 4096;
 
-    void Initialize();
-    void Cycle();
+private:
+    uint16_t pc;
+    uint8_t ram[RAM_SIZE];
+    bool screen[SCREEN_WIDTH * SCREEN_HEIGHT];
 };
