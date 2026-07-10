c01b547249bb06113ecd4b6a2567e850632cee23
Kovid Arora <kovid23arora@gmail.com>
2026-07-10 22:53:02 +0530

    todays work update

--- chip8core/include/chip8.h
+++ chip8core/include/chip8.h
@@ -5,12 +5,25 @@
 class Chip8
 {
 public:
+    Chip8();
+    static constexpr int REG_SIZE=16;
     static constexpr int SCREEN_WIDTH = 64;
     static constexpr int SCREEN_HEIGHT = 32;
     static constexpr int RAM_SIZE = 4096;
+    static constexpr int STACK_SIZE= 16;
+    static constexpr int NUM_KEYS=16;
+
 
 private:
     uint16_t pc;
     uint8_t ram[RAM_SIZE];
     bool screen[SCREEN_WIDTH * SCREEN_HEIGHT];
+    uint8_t v_reg[REG_SIZE];
+    uint16_t i_reg;
+    uint16_t stack[STACK_SIZE];
+    uint8_t sp = 0;   
+    bool keys[NUM_KEYS];
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+ 
 };
--- chip8core/src/Chip8.cpp
+++ chip8core/src/Chip8.cpp
@@ -1,16 +1,17 @@
-#include "Chip8.h"
-#include <iostream>
+#include "chip8.h"
+#include <algorithm>
 
 Chip8::Chip8()
 {
-}
+    pc = 0x200;
+    i_reg = 0;
+    sp = 0;
+    delay_timer = 0;
+    sound_timer = 0;
 
-void Chip8::Initialize()
-{
-    std::cout << "Chip8 initialized\n";
-}
-
-void Chip8::Cycle()
-{
-    std::cout << "Executing cycle\n";
+    std::fill(ram, ram + RAM_SIZE, 0);
+    std::fill(screen, screen + SCREEN_WIDTH * SCREEN_HEIGHT, false);
+    std::fill(v_reg, v_reg + REG_SIZE, 0);
+    std::fill(stack, stack + STACK_SIZE, 0);
+    std::fill(keys, keys + NUM_KEYS, false);
 }
